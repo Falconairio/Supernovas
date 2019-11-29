5d431dada64e39f755766be94190c055552815b3
stefan hranek <hranekstefan@gmail.com>
2019-11-29 13:58:35 +0100

    got data to filter

--- public/javascripts/api.js
+++ public/javascripts/api.js
@@ -1,36 +1,42 @@
-function main() { 
 
+function main() { 
+    
     const getDataForm = document.querySelector('#get-data-form');
-    // Select sections that hold the forms
-    const getIdSection = document.querySelector('#get-data-section');
-    
-    
+    // Select sections that hold the forms 
     
     getDataForm.addEventListener('submit', function(e) {
         // Prevent the form reloading of the page
         e.preventDefault();
     
     
-        // Get the id of the data from the input field
+        // // Get the id of the data from the input field
         const title = document.querySelector('#data-title').value;
     
         // If no id was provided, stop the function
     
-        if (!title) return;
+        // if (!title) return;
     
         // Make a GET request -  get one data
         axios
-            .get(`https://eonet.sci.gsfc.nasa.gov/api/v2.1/events`)
+            .get(`https://eonet.sci.gsfc.nasa.gov/api/v2/events?limit=5&days=20&status=open`)
             .then(response => {
                 // Grab all input fields from the update form (still invisible)
                 // const inputNodes = updateForm.querySelectorAll('input');
                 // const inputElements = [...inputNodes];
+                let newData = [];
+                for(let i = 0; i < response.data.events.length; i++) {
+                    if(response.data.events[i].title.includes(title)) {
+                        newData.push(response.data.events[i]);
+                    };
+                }
+                console.log(newData);
+                
                 const events = response.data.results;
-                // console.log(response.data.results);
                 const resultList = document.querySelector('.data-results');
+    console.log('THISSSSSSSSS IS WHERE THE DATA ISSSSSS',response.data);
     
                 let htmlString = "";
-                events.forEach(function(event) {
+                newData.forEach(function(event) {
                     htmlString += `
                     <div style = "
                                 display:flex; 
